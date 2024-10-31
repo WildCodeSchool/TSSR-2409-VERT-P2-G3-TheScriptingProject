ec1c53379ca764d145f64cf8c7722352e3b5df39
Julien-p31 <pighini.julien@outlook.fr>
2024-10-31 10:49:20 +0100

    Update SCRIPT-LOGICIEL.sh

--- BASH/SCRIPT-LOGICIEL.sh
+++ BASH/SCRIPT-LOGICIEL.sh
@@ -6,35 +6,38 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # Aucune couleur
 
-# Boucle while true pour la gestion de logiciel
+# Boucle while true pour faire un menu sur la gestion de logiciel
 while true; do
-    echo -e "${GREEN}------ MENU GESTION LOGICIEL ------"
+    echo -e "${GREEN}------ MENU GESTION LOGICIEL ------\n"
     echo -e "${GREEN}[1]${NC} Installer un logiciel"
     echo -e "${GREEN}[2]${NC} Désinstaller un logiciel"
     echo -e "${GREEN}[3]${NC} Quitter"
     echo -e "${GREEN}Veuillez choisir une option : ${NC}"
     read choix
 
-    # Case pour choix installation ou désinstallation logiciel
-    case $choix in 
-        1)
+    # Case pour faire un bloc avec les choix correspondants au menu 
+    case $choix in
+         # Choix pour installer un logiciel  
+         1)
             echo -e "${GREEN}Entrer le nom du logiciel à installer : ${NC}"
 	    read logiciel
             sudo apt install $logiciel -y
             ;;
-        2)
+         # Choix pour désinstaller un logiciel   
+         2)
             echo -e "${GREEN}Entrer le nom du logiciel à désinstaller : ${NC}"
             read logiciel
             sudo apt remove $logiciel -y
           ;;
-	# Sortir du script 
-        3)
+	      # Sortir du script 
+         3)
             echo -e "${YELLOW}Exit${NC}"
             break
             ;;
-	# Inique si erreur de saisie et relance le script    
-        *)
+	      # Inique si erreur de saisie et relance le script    
+         4)
             echo -e "${RED}[Erreur]! Option invalide, veuillez réessayer !${NC}"
             ;;
     esac
 done 
+# Fin du script
