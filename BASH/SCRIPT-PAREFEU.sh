a422de45a6d1aa5b02e074478132f8e22823785d
Julien-p31 <pighini.julien@outlook.fr>
2024-10-31 12:04:07 +0100

    Update SCRIPT-PAREFEU.sh

--- BASH/SCRIPT-PAREFEU.sh
+++ BASH/SCRIPT-PAREFEU.sh
@@ -9,10 +9,10 @@ NC='\033[0m' # Aucune couleur
 # Boucle while true pour faire un menu sur la gestion du pare-feu
 while true; do
     echo -e "${GREEN}------ MENU GESTION PARE-FEU ------\n"
-    echo -e "${GREEN}[1]${NC} Définir les règles de pare-feu"
-    echo -e "${GREEN}[2]${NC} Activer le pare-feu"
-    echo -e "${GREEN}[3]${NC} Désactiver le pare-feu"
-    echo -e "${GREEN}[4]${NC} Quitter"
+    echo -e "${YELLOW}[1]${NC} Définir les règles de pare-feu"
+    echo -e "${YELLOW}[2]${NC} Activer le pare-feu"
+    echo -e "${YELLOW}[3]${NC} Désactiver le pare-feu"
+    echo -e "${YELLOW}[4]${NC} Quitter"
     echo -e "${GREEN}Veuillez choisir une option : ${NC}"
     read choix
 
