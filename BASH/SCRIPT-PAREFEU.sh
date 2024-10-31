56091f2eccb5979d4ecf85ecafdf208aa989b174
Julien-p31 <pighini.julien@outlook.fr>
2024-10-31 12:04:29 +0100

    Update SCRIPT-PAREFEU.sh

--- BASH/SCRIPT-PAREFEU.sh
+++ BASH/SCRIPT-PAREFEU.sh
@@ -23,10 +23,10 @@ while true; do
          1)
 while true; do
     echo -e "${GREEN}------ MENU DES RÈGLES PARE-FEU ------\n"
-    echo -e "${GREEN}[1]${NC} Ajouter une règle"
-    echo -e "${GREEN}[2]${NC} Supprimer une règle"
-    echo -e "${GREEN}[3]${NC} Afficher les règles actuelles"
-    echo -e "${GREEN}[4]${NC} Retour au menu principal"
+    echo -e "${YELLOW}[1]${NC} Ajouter une règle"
+    echo -e "${YELLOW}[2]${NC} Supprimer une règle"
+    echo -e "${YELLOW}[3]${NC} Afficher les règles actuelles"
+    echo -e "${YELLOW}[4]${NC} Retour au menu principal"
     echo -e "${GREEN}Veuillez choisir une option : ${NC}"
     read choix_regles
     
