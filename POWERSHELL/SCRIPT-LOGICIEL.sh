1a5d93df56498f3faf0c785eb41ddb83790e8825
Julien-p31 <pighini.julien@outlook.fr>
2024-11-01 12:02:51 +0100

    Update SCRIPT-LOGICIEL.sh

--- POWERSHELL/SCRIPT-LOGICIEL.sh
+++ POWERSHELL/SCRIPT-LOGICIEL.sh
@@ -9,7 +9,7 @@ while ($true) {
     Write-Host "------ MENU GESTION LOGICIEL ------`n" -f $GREEN
     Write-Host "[1]" -f $YELLOW -nonewline; Write-Host " Installer un logiciel" -f $NC
     Write-Host "[2]" -f $YELLOW -nonewline; Write-Host " Désinstaller un logiciel" -f $NC
-    Write-Host "[3]" -f $YELLOW -nonewline; Write-Host " Quitter" -f $NC
+    Write-Host "[x]" -f $RED -nonewline; Write-Host " Quitter" -f $NC
     $choix = Read-Host "Veuillez choisir une option : "
 
     # Bloc pour choix installation ou désinstallation logiciel
