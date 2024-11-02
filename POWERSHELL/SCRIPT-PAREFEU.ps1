8747fa2219b993e83b8693e09c2d5822a47fb28d
Julien-p31 <pighini.julien@outlook.fr>
2024-11-02 11:50:50 +0100

    Update SCRIPT-PAREFEU.ps1

--- POWERSHELL/SCRIPT-PAREFEU.ps1
+++ POWERSHELL/SCRIPT-PAREFEU.ps1
@@ -6,12 +6,12 @@ $NC = "White" # Aucune couleur
 
 # Boucle while true pour faire un menu sur la gestion du pare-feu
 while ($true) {
-    Write-Host "------ MENU GESTION PARE-FEU ------`n" -ForegroundColor $GREEN
-    Write-Host "[1] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Définir les règles de pare-feu" -ForegroundColor $NC
-    Write-Host "[2] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Activer le pare-feu" -ForegroundColor $NC
-    Write-Host "[3] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Désactiver le pare-feu" -ForegroundColor $NC
-    Write-Host "[4] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Retour au menu principal" -ForegroundColor $NC
-    Write-Host "[x] " -ForegroundColor $RED -NoNewline; Write-Host "Quitter`n" -ForegroundColor $NC
+    Write-Host "------ MENU GESTION PARE-FEU ------`n" -f $GREEN
+    Write-Host "[1] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Définir les règles de pare-feu" -f $NC
+    Write-Host "[2] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Activer le pare-feu" -f $NC
+    Write-Host "[3] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Désactiver le pare-feu" -f $NC
+    Write-Host "[4] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Retour au menu principal" -f $NC
+    Write-Host "[x] " -ForegroundColor $RED -NoNewline; Write-Host "Quitter`n" -f $NC
     $choix = Read-Host "Veuillez choisir une option"
 
     # Bloc pour faire un sous-menu pour les règles du pare-feu
@@ -19,12 +19,12 @@ while ($true) {
         # Boucle while true pour faire un menu sur les règles du pare-feu
         "1" {
             while ($true) {
-                Write-Host "------ MENU DES RÈGLES PARE-FEU ------`n" -ForegroundColor $GREEN
-                Write-Host "[1] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Ajouter une règle" -ForegroundColor $NC
-                Write-Host "[2] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Supprimer une règle" -ForegroundColor $NC
-                Write-Host "[3] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Afficher les règles actuelles" -ForegroundColor $NC
-                Write-Host "[4] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Retour au menu principal" -ForegroundColor $NC
-                Write-Host "[x] " -ForegroundColor $RED -NoNewline; Write-Host "Quitter`n" -ForegroundColor $NC
+                Write-Host "------ MENU DES RÈGLES PARE-FEU ------`n" -f $GREEN
+                Write-Host "[1] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Ajouter une règle" -f $NC
+                Write-Host "[2] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Supprimer une règle" -f $NC
+                Write-Host "[3] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Afficher les règles actuelles" -f $NC
+                Write-Host "[4] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Retour au menu principal" -f $NC
+                Write-Host "[x] " -ForegroundColor $RED -NoNewline; Write-Host "Quitter`n" -f $NC
                 $choix_regles = Read-Host "Veuillez choisir une option"
 
                 switch ($choix_regles) {
