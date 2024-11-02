4fca41de05ee7dd6f05f42e8283bdbd68cbbc184
Julien-p31 <pighini.julien@outlook.fr>
2024-11-02 10:17:26 +0100

    Update SCRIPT-LOGICIEL.ps1

--- POWERSHELL/SCRIPT-LOGICIEL.ps1
+++ POWERSHELL/SCRIPT-LOGICIEL.ps1
@@ -7,10 +7,10 @@ $NC = "White" # Aucune couleur
 # Boucle while true pour la gestion de logiciel
 while ($true) {
     Write-Host "------ MENU GESTION LOGICIEL ------`n" -f $GREEN
-    Write-Host "[1]" -f $YELLOW -nonewline; Write-Host " Installer un logiciel" -f $NC
-    Write-Host "[2]" -f $YELLOW -nonewline; Write-Host " Désinstaller un logiciel" -f $NC
-    Write-Host "[3]" -f $YELLOW -nonewline; Write-Host " Retour au menu principal" -f $NC
-    Write-Host "[x]" -f $RED -nonewline; Write-Host " Quitter`n" -f $NC
+    Write-Host "[1] " -f $YELLOW -nonewline; Write-Host " Installer un logiciel" -f $NC
+    Write-Host "[2] " -f $YELLOW -nonewline; Write-Host " Désinstaller un logiciel" -f $NC
+    Write-Host "[3] " -f $YELLOW -nonewline; Write-Host " Retour au menu principal" -f $NC
+    Write-Host "[x] " -f $RED -nonewline; Write-Host " Quitter`n" -f $NC
     $choix = Read-Host "Veuillez choisir une option  "
 
     # Bloc pour choix installation ou désinstallation logiciel
