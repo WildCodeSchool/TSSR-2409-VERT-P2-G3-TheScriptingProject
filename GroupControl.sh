908338bf2ab99aebd643f5f13cdad60bec4cce00
CamilleCalvel <calvel.camille@gmail.com>
2024-11-07 18:51:21 +0100

    Update GroupControl.sh

--- GroupControl.sh
+++ GroupControl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Script projet 2 Lionel => partie utilisateur
+# Script projet 2 Lionel => partie Groupe
 # connexion ssh a une machine distante en amont via un le script MainScript.sh
+# choix des sections avec boucle while
 
 # Définir les couleurs des variables
 RED='\033[0;31m'
@@ -9,50 +10,42 @@ YELLOW='\033[0;33m'
 CYAN='\033[0;36m'
 NC='\033[0m' # Aucune couleur
 
-# choix des sections avec boucle while
-
 while true; do
-    echo -e "${GREEN}\n------ MENU COMPTE UTILISATEUR ------\n"
-    echo -e "${CYAN}[1]${NC} Créer un compte utilisateur"
-    echo -e "${CYAN}[2]${NC} Modifier un MDP"
-    echo -e "${CYAN}[3]${NC} Supprimer un compte utilisateur"
-    echo -e "${CYAN}[4]${NC} Désactiver un compte utilisateur"
-    echo -e "${CYAN}[5]${NC} Retour au menu precedent\n"
-    echo -e "Choisissez une option: "
+    echo -e "${GREEN}\n------ MENU GROUPE ------\n "
+    echo -e "${CYAN}[1]${NC} Ajout a un groupe"
+    echo -e "${CYAN}[2]${NC} Quitter le groupe"
+    echo -e "${CYAN}[3]${NC} Retour au menu précédent\n"
+    echo -e "Veuillez choisir une option :"
     read choix
-    
-# a chaque choix correspond une commande qui execute l'action demandé et boucle tant qu el'utilisateur ne choisi pas quitter"
+
+    # a chaque choix correspond une commande qui execute l'action demandé et boucle tant que l'utilisateur ne choisi pas quitter"
     case $choix in
-        1)
+
+        1)      #ajout a un groupe
+            echo -e "\nliste des groupes: "
+            groups
             echo ""
-            read -p "Quel est le nom du compte utilisateur à créer? " new_user
-            sudo adduser $new_user
+            read -p "Dans quel groupe voulez vous ajouter l'utilisateur?" add_group
+            sudo usermod -aG add_group $USER
             ;;
 
-        2)
+        2)      # quitter votre groupe
             echo ""
-            read -p "Le mdp de quel utilisateur voulez vous modifier? " userpasswd_change
-            sudo passwd $userpasswd_change
+            read -p "Voulez vous quitter votre groupe actuel? (oui ou non)" reponse
+            if [[ "$reponse" == "oui" ]]; then
+                sudo gpasswd -d $USER $(id -gn)
+                echo -e "\nvous avez bien quitter votre groupe"
+            else
+                echo -e "\nVous restez dans votre groupe actuel"
+            fi
             ;;
 
-        3)
-            echo ""
-            read -p "Quel compte utilisateur voulez supprimer? " delete_user
-            sudo userdel $delete_user
-            ;;
-
-        4)
-            echo ""
-            read -p "Quel compte utilisateur souhaitez vous désactiver? " desactivate_user
-            sudo chage -E 0 $desactivate_user
-            ;;
-
-        5) # Retour au menu précédent
+        3)      # Retour au menu précédent
             echo -e "\nRetour au menu précédent"
             break
             ;;
 
-        *)  #Erreur
+        *)      #erreur
             echo -e "${RED}\nErreur:choix invalide, veuillez recommencer"
             ;;
             
