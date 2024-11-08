3c1ca1ee8cb6111d09acc736be0535980edb6583
Julien-p31 <pighini.julien@outlook.fr>
2024-11-08 08:54:53 +0100

    Update SCRIPT-LOGICIEL.ps1

--- POWERSHELL/SCRIPT-LOGICIEL.ps1
+++ POWERSHELL/SCRIPT-LOGICIEL.ps1
@@ -9,8 +9,7 @@ while ($true) {
     Write-Host "------ MENU GESTION LOGICIEL ------`n" -f $GREEN
     Write-Host "[1] " -f $YELLOW -nonewline; Write-Host " Installer un logiciel" -f $NC
     Write-Host "[2] " -f $YELLOW -nonewline; Write-Host " Désinstaller un logiciel" -f $NC
-    Write-Host "[3] " -f $YELLOW -nonewline; Write-Host " Retour au menu principal" -f $NC
-    Write-Host "[x] " -f $RED -nonewline; Write-Host " Quitter`n" -f $NC
+    Write-Host "[3] " -f $YELLOW -nonewline; Write-Host " Retour au menu principal`n" -f $NC
     $choix = Read-Host "Veuillez choisir une option  "
 
     # Switch pour faire un bloc pour choix installation ou désinstallation logiciel
@@ -59,11 +58,6 @@ while ($true) {
             break
         }
         
-        # Sortir du script 
-        "x" {
-            Write-Host "Exit" -f $YELLOW
-            exit
-        }
         # Indique si erreur de saisie et relance le script    
         default {
             Write-Host "[ERREUR]! Option invalide, veuillez réessayer !`n" -f $RED
