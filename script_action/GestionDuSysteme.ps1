8157fdfa038744fd501a34e3ad39960a30fb3c12
CamilleCalvel <calvel.camille@gmail.com>
2024-11-11 19:06:23 +0100

    Modification de la mise en page GestionDuSysteme.ps1

--- script_action/GestionDuSysteme.ps1
+++ script_action/GestionDuSysteme.ps1
@@ -3,80 +3,79 @@ $Green = "Green"
 $Yellow = "Yellow"
 $White = "White"
 $Red = "Red"
+$Cyan= "Cyan"
 
 $statut = $true
 
 while ($statut) 
 {
-    Write-Host "MENU GESTION DU SYSTEME" -f $Green
-    Write-Host "[1]" -f $Yellow -NoNewline ; Write-Host " Arreter le systeme" -f $White;
-    Write-Host "[2]" -f $Yellow -NoNewline ; Write-Host " Redemarrer le systeme" -f $White;
-    Write-Host "[3]" -f $Yellow -NoNewline ; Write-Host " Verrouiller le systeme" -f $White;
-    Write-Host "[4]" -f $Yellow -NoNewline ; Write-Host " Mettre a jour le systeme" -f $White;
-    Write-Host "[5]" -f $Yellow -NoNewline ; Write-Host " Retourner au menu principal" -f $White;
-    Write-Host "[X]" -f $Yellow -NoNewline ; Write-Host "Quitter" -f $White;
+    Write-Host "`nMENU GESTION DU SYSTEME`n" -f $Green
+    Write-Host "[1]" -f $Cyan -NoNewline ; Write-Host " Arreter le systeme" -f $White;
+    Write-Host "[2]" -f $Cyan -NoNewline ; Write-Host " Redemarrer le systeme" -f $White;
+    Write-Host "[3]" -f $Cyan -NoNewline ; Write-Host " Verrouiller le systeme" -f $White;
+    Write-Host "[4]" -f $Cyan -NoNewline ; Write-Host " Mettre a jour le systeme" -f $White;
+    Write-Host "[5]" -f $Cyan -NoNewline ; Write-Host " Retourner au menu principal`n" -f $White;
     $choix_option = Read-Host "Veuillez choisir une option"
 
     switch ($choix_option) 
 	{
         
-		"1" 
-		{
-            $reponse = Read-Host "Voulez-vous vraiment arreter le systeme? (oui/non)"
+	"1" 
+	{
+            $reponse = Read-Host "`nVoulez-vous vraiment arreter le systeme? (oui/non)"
             while ($reponse -ne "non" -and $reponse -ne "n") 
             {
                 if ($reponse -eq "oui" -or $reponse -eq "o") 
                 {
-                    Write-Host "Le systeme est en cours d'arret..."
-                    Start-Sleep -Seconds 3
+                    Write-Host "`nLe systeme est en cours d'arret...`n"          
                     # Executer la commande d'arret du systeme
                     Stop-Computer -Force
+
                 } 
                 else 
                 {
-                    Write-Host -f $Red "Erreur lors de la saisie"
-                    $reponse = Read-Host "Voulez-vous vraiment arreter le systeme? (oui/non)"
+                    Write-Host -f $Red "`nErreur lors de la saisie"
+                    $reponse = Read-Host "`nVoulez-vous vraiment arreter le systeme? (oui/non)"
                 }
             }
-            Write-Host "Retour au menu des options du systeme"
+            Write-Host "`nRetour au menu des options du systeme"
         }
 		
         "2" 
-		{
-            $reponse = Read-Host "Voulez-vous vraiment redemarrer le systeme? (oui/non)"
+	{
+            $reponse = Read-Host "`nVoulez-vous vraiment redemarrer le systeme? (oui/non)"
             while ($reponse -ne "non" -and $reponse -ne "n") 
             {
                 if ($reponse -eq "oui" -or $reponse -eq "o") 
                 {
-                    Write-Host "Le systeme est en cours de redemarrage..."
+                    Write-Host "`nLe systeme est en cours de redemarrage..."
                     Start-Sleep -Seconds 3
                     # Executer la commande de redemarrage du systeme
                     Restart-Computer -Force
                 } 
                 else 
                 {
-                    Write-Host -f $Red "Erreur lors de la saisie"
-                    $reponse = Read-Host "Voulez-vous vraiment redemarrer le systeme? (oui/non)"
+                    Write-Host -f $Red "`nErreur lors de la saisie"
+                    $reponse = Read-Host "`nVoulez-vous vraiment redemarrer le systeme? (oui/non)"
                 }
             }
-            Write-Host "Retour au menu des options du systeme"
+            Write-Host "`nRetour au menu des options du systeme"
         }
 		
-		"3" 
-		{
-            Write-Host "Verrouillage du systeme en cours..."
-            Start-Sleep -Seconds 3
-            # Executer la commande de verrouillage
-            rundll32.exe user32.dll,LockWorkStation
+	"3" 
+	{
+            Write-Host "`nVerrouillage du systeme en cours..."
+            # Utiliser psshutdown.exe pour verrouiller la session
+            psshutdown /accepteula -l
         }
 		
         "4" 
-		{
-            Write-Host "Recherche des mises a jour disponibles..."
+	{
+            Write-Host "`nRecherche des mises a jour disponibles..."
 			Install-Module PSWindowsUpdate -Force -Scope CurrentUser
-			Write-Host "Liste des mises a jour disponibles:"
+			Write-Host "`nListe des mises a jour disponibles:"
             Get-WindowsUpdate
-            $reponse = Read-Host "Voulez-vous installer les mises a jour? (oui/non)"
+            $reponse = Read-Host "`nVoulez-vous installer les mises a jour? (oui/non)"
             while ($reponse -ne "non" -and $reponse -ne "n") 
             {
                 if ($reponse -eq "oui" -or $reponse -eq "o") 
@@ -86,21 +85,16 @@ while ($statut)
                 } 
                 else 
                 {
-                    Write-Host -f $Red "Erreur lors de la saisie"
-                    $reponse = Read-Host "Voulez-vous installer les mises a jour? (oui/non)"
+                    Write-Host -f $Red "`nErreur lors de la saisie"
+                    $reponse = Read-Host "`nVoulez-vous installer les mises a jour? (oui/non)"
                 }
             }
-            Write-Host "Retour au menu des options du systeme"
+            Write-Host "`nRetour au menu des options du systeme"
         }
 		
-		"5" 
-		{
+	"5" 
+	{
             $statut = $false
         }
-		
-		"X" 
-		{
-			exit 0
-		}
     }
 }
