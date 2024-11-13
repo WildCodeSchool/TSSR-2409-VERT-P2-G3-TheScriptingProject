42bca0913da2539b247c5972d54a4311f81d279e
CamilleCalvel <calvel.camille@gmail.com>
2024-11-14 00:59:15 +0100

    Ajout des logs + correction couleur des menus

--- script_information/InfoDuSystemeSoftware.ps1
+++ script_information/InfoDuSystemeSoftware.ps1
@@ -4,18 +4,34 @@
 $RED = "Red"
 $GREEN = "Green"
 $YELLOW = "Yellow"
+$CYAN = "Cyan"
 $NC = "White" # Aucune couleur
+
+# Fonction log
+$Logfile = "C:\Windows\Temp\log-remote.log"
+function WriteLog
+{
+Param ([string]$LogString)
+$Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+$User = $env:USERNAME
+$LogMessage = "$Stamp-$User-$LogString"
+Add-content $LogFile -value $LogMessage
+}
+
+# log lancement du script
+WriteLog "********StartScriptInfoDuSystemeSoftware********"
 
 # fonction récupération de la version du système d'exploitation
 function showOsVersion {
 
     Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property Caption, Version, BuildNumber
-
+     WriteLog "Consultation de la version du systeme d'exploitation"
 }
 # fonction de récupération du nombre de disques présents
 function showNbDisks {
 
     Get-PhysicalDisk | Measure-Object | Select-Object -ExpandProperty Count
+    WriteLog "Consultation du nombre de disques presents"
 
 }
 
@@ -23,18 +39,19 @@ function showNbDisks {
 function showPartsByDisks {
 
     Get-Partition
+    WriteLog "Consultation des partitions par disque"
 }
 
 # fonction de récupération des paquets ou des applications installés
 function showInstalledAppPackages {
 
-
+    WriteLog "Navigation dans le menu d'affichage des executables"
     Write-Host "<< AFFICHER DES EXECUTABLES >>" -f $GREEN
     Write-Host "`nSouhaitez-vous afficher les paquets installes ou les applications installees" -f $GREEN
     
-    Write-Host "[1]" -f $YELLOW -NoNewline; Write-Host "Paquets installes" -f $NC
-    Write-Host "[2]" -f $YELLOW -NoNewline; Write-Host "Applications installees" -f $NC
-    Write-Host "[3]" -f $YELLOW -NoNewline; Write-Host "Retour au menu precedent" -f $NC
+    Write-Host "[1]" -f $CYAN -NoNewline; Write-Host "Paquets installes" -f $NC
+    Write-Host "[2]" -f $CYAN -NoNewline; Write-Host "Applications installees" -f $NC
+    Write-Host "[3]" -f $CYAN -NoNewline; Write-Host "Retour au menu precedent" -f $NC
     Write-Host "Choisir le chiffre correspondant à l'option:" -f $GREEN
     
     $userChoiceInfoApp = Read-Host
@@ -45,20 +62,22 @@ function showInstalledAppPackages {
             # Liste uniquement les paquets installés
             Write-Host "Liste des paquets installes :"
             Get-Package
+            WriteLog "Consultation de la liste des paquets installes"
             
         } elseif ($userChoiceInfoApp -eq "2") {
             # Liste les applications graphiques
             Write-Host "Liste des applications installees :"
             Get-StartApps | Select-Object Name
+            WriteLog "Consultation de la liste des applications installees"
             
         } else {
             Write-Host "`nVeuillez saisir une option valide"
         }
 
         Write-Host "`nSouhaitez-vous afficher a nouveau les paquets ou applications ?" -f $GREEN
-        Write-Host "[1]" -f $YELLOW -NoNewline; Write-Host "Paquets installes" -f $NC
-        Write-Host "[2]" -f $YELLOW -NoNewline; Write-Host "Applications installees" -f $NC
-        Write-Host "[3]" -f $YELLOW -NoNewline; Write-Host "Retour au menu precedent" -f $NC
+        Write-Host "[1]" -f $CYAN -NoNewline; Write-Host "Paquets installes" -f $NC
+        Write-Host "[2]" -f $CYAN -NoNewline; Write-Host "Applications installees" -f $NC
+        Write-Host "[3]" -f $CYAN -NoNewline; Write-Host "Retour au menu precedent" -f $NC
         
         $userChoiceInfoApp = Read-Host
     }
@@ -70,6 +89,7 @@ function showInstalledAppPackages {
 function showRunningServices {
 
     Get-Service | Where-Object { $_.Status -eq 'Running' } | Select-Object Name
+    WriteLog "Consultation de la liste des services en cours d'execution"
 }
 
 # fonction de récupération des utilisateurs locaux
@@ -77,6 +97,7 @@ function showLocalUsers {
 
     $users = Get-LocalUser | Select-Object Name, Enabled, Description
     $users
+    WriteLog "Consultation de la liste des utilisateurs locaux"
 }
 
 # menu de sélection des informations relatives à la couche OS - software
@@ -85,13 +106,13 @@ $userChoiceDir = ""
 
 while ($userChoiceDir -ne "x") {
     Write-Host "`n------INFORMATIONS OS , DISKS, APP & LOCALUSERS------" -f $GREEN
-    Write-Host "[1]" -f $YELLOW -NoNewline; Write-Host "Afficher la version de l'OS" -f $NC
-    Write-Host "[2]" -f $YELLOW -NoNewline; Write-Host "Afficher le nombre de disques" -f $NC
-    Write-Host "[3]" -f $YELLOW -NoNewline; Write-Host "Afficher les partitions par disque" -f $NC
-    Write-Host "[4]" -f $YELLOW -NoNewline; Write-Host "Afficher la liste des applications/paquets installes" -f $NC
-    Write-Host "[5]" -f $YELLOW -NoNewline; Write-Host "Afficher la liste des services en cours d'execution" -f $NC
-    Write-Host "[6]" -f $YELLOW -NoNewline; Write-Host "Afficher la liste des utilisateurs locaux" -f $NC
-    Write-Host "[7]" -f $YELLOW -NoNewline; Write-Host "Retour au menu precedent" -f $NC
+    Write-Host "[1]" -f $CYAN -NoNewline; Write-Host "Afficher la version de l'OS" -f $NC
+    Write-Host "[2]" -f $CYAN -NoNewline; Write-Host "Afficher le nombre de disques" -f $NC
+    Write-Host "[3]" -f $CYAN -NoNewline; Write-Host "Afficher les partitions par disque" -f $NC
+    Write-Host "[4]" -f $CYAN -NoNewline; Write-Host "Afficher la liste des applications/paquets installes" -f $NC
+    Write-Host "[5]" -f $CYAN -NoNewline; Write-Host "Afficher la liste des services en cours d'execution" -f $NC
+    Write-Host "[6]" -f $CYAN -NoNewline; Write-Host "Afficher la liste des utilisateurs locaux" -f $NC
+    Write-Host "[7]" -f $CYAN -NoNewline; Write-Host "Retour au menu precedent" -f $NC
     Write-Host "Choisir le chiffre correspondant a l'option:" -f $GREEN
     $userChoiceDir = Read-Host
 
@@ -105,7 +126,7 @@ while ($userChoiceDir -ne "x") {
             Write-Host "`nAppuyez sur [Entrée] pour afficher la liste d'utilisateurs..."
             Read-Host         
             }
-        "7" { Write-Host "Retour au menu precedent" }
+        "7" { Write-Host "Retour au menu precedent"; WriteLog "********EndScriptInfoDuSystemeSoftware********" }
         default { Write-Host "`nVeuillez choisir une option valide`n" }
     }
 }
