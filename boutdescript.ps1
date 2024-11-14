b3a24a2b93528bf388b4076e9dd60c58273eee36
osmc2017 <bartairelionel@gmail.com>
2024-11-14 10:11:43 +0100

    addgroup

--- boutdescript.ps1
+++ boutdescript.ps1
@@ -1,19 +1,28 @@
-function UsersDesactivate {
-    # Obtenir la liste des utilisateurs locaux
-    $users = Get-LocalUser | Select-Object -ExpandProperty Name
-    Write-Host "Liste des utilisateurs :"
+# Fonction pour ajouter un utilisateur à un groupe
+function GroupAdd {
+    # Demander le nom du groupe
+    $group = Read-Host "Dans quel groupe voulez-vous ajouter l'utilisateur ?"
     
-    # Afficher chaque utilisateur
-    $users | ForEach-Object { Write-Host $_ }
-    
-    # Demander à l'utilisateur quel compte désactiver
-    $desactivate_user = Read-Host "Quel compte utilisateur souhaitez-vous désactiver ?"
-    
-    # Vérifier si le compte existe et désactiver l'utilisateur
-    if ($users -contains $desactivate_user) {
-        Disable-LocalUser -Name $desactivate_user
-        Write-Host "Compte utilisateur $desactivate_user désactivé."
+    # Vérifier si le groupe existe
+    if (Get-LocalGroup -Name $group -ErrorAction SilentlyContinue) {
+        # Obtenir le nom de l'utilisateur actuel
+        $username = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+        
+        # Ajouter l'utilisateur au groupe
+        try {
+            Add-LocalGroupMember -Group $group -Member $username
+            Write-Host "Ajout de $username au groupe $group réussi."
+            
+            # Vérifier si la fonction WriteLog est définie, puis appeler WriteLog si elle existe
+            if (Get-Command -Name WriteLog -ErrorAction SilentlyContinue) {
+                WriteLog "Ajout de $username au groupe $group"
+            } else {
+                Write-Host "Log non enregistré : la fonction WriteLog est introuvable."
+            }
+        } catch {
+            Write-Host "Erreur lors de l'ajout de l'utilisateur au groupe : $_"
+        }
     } else {
-        Write-Host "Utilisateur non trouvé."
+        Write-Host "Le groupe '$group' n'existe pas."
     }
 }
