be46789d7b43c805aff601b41add8852b7017364
Julien-p31 <pighini.julien@outlook.fr>
2024-11-15 11:55:09 +0100

    Update GestionLogiciel.ps1

--- script_action/GestionLogiciel.ps1
+++ script_action/GestionLogiciel.ps1
@@ -22,18 +22,18 @@ WriteLog "Navigation dans le menu de gestion de logiciel"
 
 # Boucle while true pour le menu gestion de logiciel
 while ($true) {
-    Write-Host "`n------ MENU GESTION LOGICIEL ------`n" -f $GREEN
+    Write-Host "`n<=== MENU GESTION DU LOGICIEL ===>`n" -f $GREEN
     Write-Host "[1] " -f $CYAN -nonewline; Write-Host " Installer un logiciel" -f $NC
-    Write-Host "[2] " -f $CYAN -nonewline; Write-Host " Desinstaller un logiciel" -f $NC
+    Write-Host "[2] " -f $CYAN -nonewline; Write-Host " Désinstaller un logiciel" -f $NC
     Write-Host "[3] " -f $CYAN -nonewline; Write-Host " Retour au menu principal`n" -f $NC
-    $choix = Read-Host "Veuillez choisir une option"
+    $choix = Read-Host "Veuillez choisir une option "
 
     # Switch pour les choix du menu
     switch ($choix) {
     
         # Installer un logiciel
         "1" {
-            Write-Host "Entrer le nom du logiciel a installer : `n" -f $GREEN 
+            Write-Host "`nEntrer le nom du logiciel à installer : `n" -f $GREEN 
             $logiciel = Read-Host "Nom du logiciel "
 
             Write-Host ""
@@ -43,8 +43,8 @@ while ($true) {
 
             # Vérification de la procédure d'installation de logiciel
             if ($process.ExitCode -eq 0) {
-            Write-Host "`n$logiciel a ete installe avec succes.`n" -f $GREEN
-            WriteLog "$logiciel a ete installe avec succes"
+            Write-Host "`nLe logiciel '$logiciel' a été installé avec succés.`n" -f $GREEN
+            WriteLog "$logiciel a été installé avec succés"
             } 
               else {
                    Write-Host "`nErreur lors de l'installation de $logiciel.`n" -f $RED
@@ -53,8 +53,8 @@ while ($true) {
 
         # Désinstaller un logiciel
         "2" {
-            Write-Host "Entrer le nom du logiciel a desinstaller : `n" -f $GREEN
-            $logiciel = Read-Host "Nom du logiciel"
+            Write-Host "`nEntrer le nom du logiciel à désinstaller : `n" -f $GREEN
+            $logiciel = Read-Host "Nom du logiciel "
 
             Write-Host ""
 
@@ -63,18 +63,18 @@ while ($true) {
 
             # Vérification de la procédure de désinstallation de logiciel
             if ($process.ExitCode -eq 0) {
-            Write-Host "`n$logiciel a ete desinstalle avec succes.`n" -f $GREEN
-            WriteLog "$logiciel a ete desinstalle avec succes"
+            Write-Host "`nLe logiciel '$logiciel' à été désinstallé avec succés.`n" -f $GREEN
+            WriteLog "$logiciel à été désinstallé avec succés"
             }
                 else {
-                Write-Host "`nErreur lors de la desinstallation de $logiciel.`n" -f $RED
+                Write-Host "`nErreur lors de la désinstallation de $logiciel.`n" -f $RED
             }
         }
         
         # Retour au menu principal 
         "3" {
             WriteLog "********EndScriptGestionLogiciel********"
-            break
+            return
         }
         
         # Indique si erreur de saisie et relance le script    
